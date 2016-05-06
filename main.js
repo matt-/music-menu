765b7c84b9652219b45879925ba1d87717cb4054
Matt Austin <matt@m-austin.com>
2016-05-05 21:39:18 -0700

    init commit

---
+++ main.js
@@ -0,0 +1,77 @@
+"use strict";
+var electron = require('electron'),
+	path = require('path'),
+	Positioner = require('electron-positioner');
+
+var BrowserWindow = electron.BrowserWindow;
+var globalShortcut = electron.globalShortcut;
+var Menu = electron.Menu;
+var Tray = electron.Tray;
+
+// Module to control application life.
+var app = electron.app;
+var currentSong = {};
+
+app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, '/PepperFlash/PepperFlashPlayer.plugin'));
+
+app.on('ready', function() {
+	// Menubar icon
+	var appIcon = new Tray(path.join(__dirname, 'headphones-with-music-note.png'));
+
+	// RPC to handle events from the page.
+	electron.ipcMain.on('asynchronous-message', function(event, arg){
+		if (arg.title) {
+			currentSong = arg
+			appIcon.setToolTip(currentSong.title + ' by ' + currentSong.artist)
+		}
+	});
+
+	// Menu bar right click menu
+	var contextMenu = Menu.buildFromTemplate([{
+		label: 'Quit',
+		accelerator: 'Command+Q',
+		click: function click() {
+			app.quit();
+		}
+	}]);
+	// Menu bar actions
+	appIcon.on('right-click', function () {
+		appIcon.popUpContextMenu(contextMenu);
+	});
+
+	appIcon.on('click', function () {
+		if (mainWindow.isVisible()) {
+			mainWindow.hide()
+		} else {
+			mainWindow.show()
+		}
+	});
+
+	// Create the browser window.
+	var mainWindow = new BrowserWindow({
+		width: 1000,
+		height: 600,
+		webPreferences: {
+			plugins: true,
+			// nodeIntegration on the pare are break jquery do everything in a preload
+			nodeIntegration: false,
+			preload: path.join(__dirname, 'pandora_preload.js')
+		},
+		frame: false
+	});
+	var positioner = new Positioner(mainWindow);
+	positioner.move('topRight');
+
+	mainWindow.loadURL('http://pandora.com/', {
+		userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36'
+	});
+
+	// Set up media keys
+	globalShortcut.register('MediaPlayPause', function () {
+		mainWindow.webContents.executeJavaScript('Play();')
+	})
+	globalShortcut.register('MediaNextTrack', function () {
+		mainWindow.webContents.executeJavaScript('Next();')
+	})
+
+})
---
+++ pandora_preload.js
@@ -0,0 +1,53 @@
+window.addEventListener('load', function() {
+	var ipcRenderer = require('electron').ipcRenderer;
+	window.simulateClick = function simulateClick(element) {
+		if (!element) {
+			return false;
+		}
+		var click = new MouseEvent('click', {
+			bubbles: true,
+			cancelable: false,
+			view: window,
+		});
+		return element.dispatchEvent(click);
+	}
+	window.Play = function Play() {
+		var isPlaying = document.querySelector('div.playButton').style
+				.display !== 'block';
+		var playPauseButton = null;
+		if (isPlaying) {
+			playPauseButton = document.querySelector(
+				'div.pauseButton > a');
+		} else {
+			playPauseButton = document.querySelector(
+				'div.playButton > a');
+		}
+		simulateClick(playPauseButton);
+	}
+	window.Next = function Next() {
+		skipButton = document.querySelector('div.skipButton > a');
+		simulateClick(skipButton);
+	}
+	var lastTitle = ''
+	setInterval(function() {
+		var title = document.querySelector('.playerBarSong').textContent;
+		var artist = document.querySelector('.playerBarArtist')
+			.textContent;
+		var album = document.querySelector('.playerBarAlbum').textContent;
+		var art = document.querySelector('.playerBarArt').src
+		// We got a new song!
+		if (title != lastTitle) {
+			var note = new Notification(title, {
+				silent: true,
+				body: 'by ' + artist + '\non ' + album,
+				icon: art
+			});
+			lastTitle = title;
+			ipcRenderer.send('asynchronous-message', {
+				title: title,
+				artist: artist,
+				album: album
+			});
+		}
+	}, 1000);
+}, false);
